c87bfba8e28c21ba9a4adeca7ef85137f3305085
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-12 12:50:49 +0100

    Add actors list to page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -24,8 +24,34 @@ export default function Main() {
 	useEffect(() => console.log(actors, actresses), [actors, actresses]);
 
 	return (
-		<>
-			<div className="container"></div>
-		</>
+		<div className="container">
+			<ul className="actors-list">
+				{actors.length > 0
+					? actors.map((actor) => (
+							<li key={actor.id}>
+								<div className="card">
+									<img src={actor.image} alt={actor.name} />
+									<div className="card-body">
+										<h3>{actor.name}</h3>
+										<div>{actor.birth_year}</div>
+										<div>
+											<span className="bold">Nationality: </span>
+											{actor.nationality}
+										</div>
+										<div>
+											<span className="bold">Biography: </span>
+											{actor.biography}
+										</div>
+										<div>
+											<span className="bold">Awards: </span>
+											{actor.awards.join(", ")}
+										</div>
+									</div>
+								</div>
+							</li>
+						))
+					: ""}
+			</ul>
+		</div>
 	);
 }
