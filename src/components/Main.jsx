d82834d7308f470a7a39aa1dc79fc6944dbc3d30
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-12 12:19:35 +0100

    Add fetch logic

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,31 @@
-export default function Main() {
-	const actorsEndpoint = "https://lanciweb.github.io/demo/api/actresses/";
-	const actressesEndpoint = "https://lanciweb.github.io/demo/api/actors/";
+import { useEffect, useState } from "react";
 
-	return <></>;
+export default function Main() {
+	const actorsEndpoint = "https://lanciweb.github.io/demo/api/actors/";
+	const actressesEndpoint = "https://lanciweb.github.io/demo/api/actresses/";
+
+	const [actors, setActors] = useState([]);
+	const [actresses, setActresses] = useState([]);
+
+	function fetchActors() {
+		fetch(actorsEndpoint)
+			.then((res) => res.json())
+			.then(setActors)
+			.catch((error) => console.error(error.message));
+
+		fetch(actressesEndpoint)
+			.then((res) => res.json())
+			.then(setActresses)
+			.catch((error) => console.error(error.message));
+	}
+
+	useEffect(fetchActors, []);
+
+	useEffect(() => console.log(actors, actresses), [actors, actresses]);
+
+	return (
+		<>
+			<div className="container"></div>
+		</>
+	);
 }
